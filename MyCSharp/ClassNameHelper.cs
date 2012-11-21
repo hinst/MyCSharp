810b1016d31d6d9c2e79ff1a6bdfd0238b5c5e29
Alexander Savinoff <hinstance@yandex.ru>
2012-11-21 13:57:27 +0400

    up

--- MyCSharp/ClassNameHelper.cs
+++ MyCSharp/ClassNameHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace MYCSHARP
+namespace MyCSharp
 {
 
     public static class ClassNameHelper
---
+++ MyNLog/NLogContainer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NLog;
+
+using MyCSharp;
+
+namespace MyNLog
+{
+
+	class NLogContainer
+	{
+
+		public class LoggerDictionary : Dictionary<object, Logger>
+		{
+		}
+
+		protected static LoggerDictionary loggers;
+
+		// auto-create property
+		protected LoggerDictionary getLoggers()
+		{
+			if (loggers == null)
+				loggers = new LoggerDictionary();
+			return loggers;
+		}
+
+		protected LoggerDictionary Loggers
+		{
+			get
+			{
+				return getLoggers();
+			}
+		}
+
+		public Logger Get(object x)
+		{
+			Logger result = null;
+			if (Loggers.ContainsKey(x))
+				result = Loggers[x];
+			else
+			{
+				result = LogManager.GetLogger(x.GetFullClassName());
+				Loggers.Add(x, result);
+			}
+			return result;
+		}
+
+	}
+
+}
