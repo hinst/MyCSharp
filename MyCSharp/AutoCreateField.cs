290b52199b5ea9b6af4e11195d816fa0d54df64e
Alexander Savinoff <hinstance@yandex.ru>
2013-02-04 14:03:01 +0400

    Добавил новую штуковину для полей которые должны создаваться по первому обращению: класс AutoCreateField

---
+++ MyCSharp/AutoCreateField.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp
+{
+
+	public static class AutoCreateField
+	{
+
+		public static T Get<T>(ref T item, Func<T> create)
+		{
+			if (null == item)
+				item = create();
+			return item;
+		}
+
+	}
+
+}
