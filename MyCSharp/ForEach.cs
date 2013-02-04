3af425cf0b844ee6f720e57cdf1181a075d8d2da
Alexander Savinoff <hinstance@yandex.ru>
2013-02-04 21:12:30 +0400

    Придумал класс ForEach с методом WhoIs который выполняет действия для всех элементов списка, которые имеют заданный тип

---
+++ MyCSharp/ForEach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp
+{
+
+	public static class ForEach
+	{
+
+		public static int WhoIs<T>(IEnumerable<object> enumerable, Action<T> action) where T: class
+		{
+			int count = 0;
+			foreach (object item in enumerable)
+			{
+				var itemOfT = item as T;
+				if (itemOfT != null)
+					action(itemOfT);
+			}
+			return count;
+		}
+
+	}
+
+}
