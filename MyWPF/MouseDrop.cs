e28b1b908283d7639b4fbad7f00de20e5ec91661
Alexander Savinoff <hinstance@yandex.ru>
2013-02-06 19:21:14 +0400

    Начал работу над классом MouseDrop, который должен обеспечить возможность "уронить" мышью что либо на что либо

---
+++ MyWPF/MouseDrop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyWPF
+{
+
+	public class MouseDrop
+	{
+
+		public void Create(FrameworkElement target, Cursor cursor)
+		{
+			this.target = target;
+			Target.Cursor = cursor;
+			Target.PreviewMouseDown += PreviewTargetMouseDown;
+		}
+
+		protected FrameworkElement target;
+
+		public FrameworkElement Target
+		{
+			get
+			{
+				return target;
+			}
+		}
+
+		void PreviewTargetMouseDown(object sender, MouseButtonEventArgs e)
+		{
+		}
+
+	}
+
+
+}
